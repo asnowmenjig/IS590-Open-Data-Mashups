7a3e0a80bfd35798c1c5685b681835e1124cbdbd
Jasmine W <jasminewong@comcast.net>
2019-10-18 15:30:19 -0500

    updating estc_extraction code

---
+++ dataset-inventory/estc/2019-10-18/estc_extraction.py
@@ -0,0 +1,34 @@
+import json
+
+infile = open('estc_sample.json', 'r', encoding = 'utf-8')
+data = json.load(infile)
+infile.close()
+
+author_dictionary = {}
+
+for record in data:
+    if 'author' in record:
+        author_name = record['author']
+    else:
+        author_name = 'missing_author'
+
+    if author_name in author_dictionary:
+        author_dictionary[author_name] += 1
+    else:
+        author_dictionary[author_name] = 1
+
+print(author_dictionary)
+
+
+
+import csv
+
+outfile = open('estc_dataset.csv', 'w', newline = '')
+csvout = csv.writer(outfile)
+csvout.writerow(['author', 'count'])
+
+for pair in author_dictionary.items():
+    label = pair[0]
+    count = pair[1]
+    row = [label, count]
+    csvout.writerow(row)
