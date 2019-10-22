f20dd563f6cab13722ee706d4b30e7c80016cd4a
Jasmine W <jasminewong@comcast.net>
2019-10-21 21:39:51 -0500

    testing code

--- dataset-inventory/project-gutenberg-2/bookfiles_2019-10-8/bookfiles/project-gutenberg-2_extract_loop.py
+++ dataset-inventory/project-gutenberg-2/bookfiles_2019-10-8/bookfiles/project-gutenberg-2_extract_loop.py
@@ -13,8 +13,8 @@ file_counter = 1
 #pg2.rdf = pg37942.rdf = 2 authors
 
 
-while file_counter < 3:
-    print('pg'+str(file_counter)+'.rdf')
+while file_counter < 1001:
+    # print('pg'+str(file_counter)+'.rdf')
     with open('pg'+str(file_counter)+'.rdf', 'rb') as xmlin:
         text = xmlin.read()
 
@@ -31,12 +31,39 @@ while file_counter < 3:
         else:
             deathdate_list.append(clean(creator.xpath('./pgterms:agent/pgterms:deathdate/text()', namespaces = root.nsmap)))
 
-    print('redefining file_counter')
+    # print('redefining file_counter')
     file_counter = file_counter + 1
 
 print("Authors list created!")
 
-print(authors_list)
-# print(len(authors_list))
-print(birthdate_list)
-print(deathdate_list)
+# print(authors_list)
+# print(birthdate_list)
+# print(deathdate_list)
+
+print('authors_list = ' + str(len(authors_list)))
+print('birthdate_list = ' + str(len(birthdate_list)))
+print('deathdate_list = ' + str(len(deathdate_list)))
+
+
+
+
+### writing to CSV file ###
+
+index_counter = 0
+
+import csv
+
+outfile = open('project-gutenberg-2_dataset.csv', 'w', encoding = 'utf-8', newline = '')
+csvout = csv.writer(outfile)
+csvout.writerow(['author', 'birthdate', 'deathdate'])
+
+for item in authors_list:
+    author = authors_list[index_counter]
+    birthdate = birthdate_list[index_counter]
+    deathdate = deathdate_list[index_counter]
+    row = [author, birthdate, deathdate]
+    csvout.writerow(row)
+    index_counter = index_counter + 1
+
+outfile.close()
+print("Success: data written and file closed!")
