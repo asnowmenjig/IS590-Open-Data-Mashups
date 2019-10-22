c0045d6e9e99e48f85ed6ae5464ebf6e41c2a3ca
Jasmine W <jasminewong@comcast.net>
2019-10-21 21:17:58 -0500

    fixed code for single rdf file

--- dataset-inventory/project-gutenberg-2/testing_tar/project-gutenberg-2_extract.py
+++ dataset-inventory/project-gutenberg-2/testing_tar/project-gutenberg-2_extract.py
@@ -4,47 +4,53 @@ with open('pg37942.rdf', 'rb') as xmlin:
     text = xmlin.read()
 
 #pg37942.rdf = 2 authors
-#pg1041.rdf = 1 author (shakesepare)
+#pg1041.rdf = 1 author
 
 root = etree.fromstring(text)
-
-# define namespace
-
-ns = {'xmlns:dcterms':"http://purl.org/dc/terms/",
-      'xmlns:rdfs':"http://www.w3.org/2000/01/rdf-schema#",
-      'xmlns:dcam':"http://purl.org/dc/dcam/",
-      'xmlns:pgterms':"http://www.gutenberg.org/2009/pgterms/",
-      'xmlns:rdf':"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-      'xmlns:cc':"http://web.resource.org/cc/"
-      }
 
 # cleaning function
 def clean(l):
     return " ".join(" ".join(l).split())
 
 authors_list = []
+birthdate_list = []
+deathdate_list = []
 
 # adding author names to author_names
-for creator in root.xpath('//dc:creator', namespaces = ns):
-    # print("Title:", end = "")
-    # print(clean(creator.xpath('../dc:title/text()', namespaces = root.nsmap)))
-    if len(creator) == 0:
-        authors_list.append(clean(creator.xpath('./text()', namespaces = ns)))
+for creator in root.xpath('//dcterms:creator', namespaces = root.nsmap):
+    authors_list.append(clean(creator.xpath('./pgterms:agent/pgterms:name/text()', namespaces = root.nsmap)))
+    if len(creator.xpath('./pgterms:agent/pgterms:birthdate/text()', namespaces = root.nsmap)) == 0:
+        birthdate_list.append("no_birthdate")
     else:
-        for each in creator.xpath('rdf:Bag/rdf:li', namespaces = ns):
-            authors_list.append(clean(each.xpath('./text()')))
+        birthdate_list.append(clean(creator.xpath('./pgterms:agent/pgterms:birthdate/text()', namespaces = root.nsmap)))
+    if len(creator.xpath('./pgterms:agent/pgterms:deathdate/text()', namespaces = root.nsmap)) == 0:
+        deathdate_list.append("no_deathdate")
+    else:
+        deathdate_list.append(clean(creator.xpath('./pgterms:agent/pgterms:deathdate/text()', namespaces = root.nsmap)))
 
 
-root.xpath('//dc:creator', namespaces = root.nsmap)
+    # print("Title:", end = "")
+    # print(clean(creator.xpath('./pgterms:agent/pgterms:name/text()', namespaces = root.nsmap)))
+    # if len(creator) == 0:
+    #     authors_list.append(clean(creator.xpath('./pgterms:agent/pgterms:name/text()', namespaces = root.nsmap)))
+    # else:
+    #     for each in creator.xpath('rdf:Bag/rdf:li', namespaces = root.nsmap):
+    #         authors_list.append(clean(each.xpath('../pgterms:agent/pgterms:name/text()')))
+
+# root.xpath('//dcterms:creator', namespaces = root.nsmap)
 
 print("Authors list created!")
 
-# print(authors_list)
-print(len(authors_list))
+print(authors_list)
+# print(len(authors_list))
+print(birthdate_list)
+print(deathdate_list)
 
 
-### writing to CSV file ###
 
+
+# ### writing to CSV file ###
+#
 # import csv
 #
 # with open("project-gutenberg_dataset.csv", 'w', newline = '', encoding = "UTF-8") as outfile_csv:
