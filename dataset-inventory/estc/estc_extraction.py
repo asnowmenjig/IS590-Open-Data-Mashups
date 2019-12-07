020fbcefe58af1853a8e59af9e11c874342a4588
Jasmine W <jasminewong@comcast.net>
2019-12-07 15:13:52 -0600

    adding code comments

--- dataset-inventory/estc/estc_extraction.py
+++ dataset-inventory/estc/estc_extraction.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open('estc_for-parsing.json', 'rb')
+infile = open('estc_for-parsing.json', 'rb')        #import data and close infile
 data = json.load(infile)
 infile.close()
 
@@ -10,30 +10,28 @@ print('building author_dictionary')
 
 for record in data:
     if 'author' in record:
-        author_name = record['author']
-    else:
-        author_name = 'missing_author'
+        author_name = record['author']              #if object has an author name field, set author_name variable to
+    else:                                           #  the string contained in that field. if no author name, set
+        author_name = 'missing_author'              #  variable to 'missing_author'
 
-    if author_name in author_dictionary:
-        author_dictionary[author_name] += 1
-    else:
+    if author_name in author_dictionary:            #if the string assigned to author_name variable is already in the
+        author_dictionary[author_name] += 1         #  counting dictionary, count +1. if not, add to the dictionary
+    else:                                           #  and assign count=1
         author_dictionary[author_name] = 1
-
-# print(author_dictionary)
 
 print("writing to csv")
 
 import csv
 
-outfile = open('estc_dataset.csv', 'w', encoding = 'utf-8', newline = '')
+outfile = open('estc_dataset.csv', 'w', encoding = 'utf-8', newline = '')       #create csv to hold extracted data
 csvout = csv.writer(outfile)
 csvout.writerow(['author', 'count'])
 
-for pair in author_dictionary.items():
-    label = pair[0]
+for pair in author_dictionary.items():              #for each row in csv, write author names into first column and
+    label = pair[0]                                 #  counts into second column
     count = pair[1]
     row = [label, count]
     csvout.writerow(row)
 
-outfile.close()
+outfile.close()                                     #close outfile
 print("Success: data written and file closed!")
