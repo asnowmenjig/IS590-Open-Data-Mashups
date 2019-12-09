0122b78e4d1af3fa244e093edad0de6be195722b
Jasmine W <jasminewong@comcast.net>
2019-12-08 21:31:26 -0600

    adding code comments

--- dataset-inventory/project-gutenberg-2/project-gutenberg-2_extract_loop.py
+++ dataset-inventory/project-gutenberg-2/project-gutenberg-2_extract_loop.py
@@ -1,23 +1,23 @@
-def clean(l):
+def clean(l):                                       #cleaning function
     return " ".join(" ".join(l).split())
 
 from lxml import etree
 
-authors_list = []
-birthdate_list = []
-deathdate_list = []
+authors_list = []                                   #list for author name values
+birthdate_list = []                                 #list for birthdate values
+deathdate_list = []                                 #list for deathdate values
 
-file_counter = 1
+file_counter = 1                                    #file counter value
 
 
-while file_counter < 5:                         #change to 60453
+while file_counter < 60453:#total number of files
     if file_counter == 1691:
         file_counter = file_counter + 1
     elif file_counter == 28520:
         file_counter = file_counter + 1
-    elif file_counter == 30254:
-        file_counter = file_counter + 1
-    elif file_counter == 30360:
+    elif file_counter == 30254:                     #if-elif chunk:
+        file_counter = file_counter + 1             #  skipping missing files; added through hard-coding when
+    elif file_counter == 30360:                     #  loop hit an error
         file_counter = file_counter + 1
     elif file_counter == 36169:
         file_counter = file_counter + 1
@@ -31,25 +31,25 @@ while file_counter < 5:                         #change to 60453
         file_counter = file_counter + 1
     elif file_counter == 60001:
         file_counter = file_counter + 1
-    # print('pg'+str(file_counter)+'.rdf')
-    with open('pg'+str(file_counter)+'.rdf', 'rb') as xmlin:
+
+    with open('pg'+str(file_counter)+'.rdf', 'rb') as xmlin:        #looping through RDF files and reading individually
         text = xmlin.read()
 
     root = etree.fromstring(text)
 
-    for creator in root.xpath('//dcterms:creator', namespaces = root.nsmap):
-        authors_list.append(clean(creator.xpath('./pgterms:agent/pgterms:name/text()', namespaces = root.nsmap)))
+    for creator in root.xpath('//dcterms:creator', namespaces = root.nsmap):                #look at creator namespace
+        authors_list.append(clean(creator.xpath('./pgterms:agent/pgterms:name/text()', namespaces = root.nsmap)))       #add author name to authors_list
         if len(creator.xpath('./pgterms:agent/pgterms:birthdate/text()', namespaces = root.nsmap)) == 0:
-            birthdate_list.append("no_birthdate")
-        else:
+            birthdate_list.append("no_birthdate")                                                                                   #if birthdate absent, add "no_birthdate" to birthdate_list;
+        else:                                                                                                                       #otherwise add birthdate value
             birthdate_list.append(clean(creator.xpath('./pgterms:agent/pgterms:birthdate/text()', namespaces = root.nsmap)))
         if len(creator.xpath('./pgterms:agent/pgterms:deathdate/text()', namespaces = root.nsmap)) == 0:
-            deathdate_list.append("no_deathdate")
-        else:
+            deathdate_list.append("no_deathdate")                                                                                   #if deathdate absent, add "no_deathdate" to deathdate_list
+        else:                                                                                                                       #otherwise add deathdate value
             deathdate_list.append(clean(creator.xpath('./pgterms:agent/pgterms:deathdate/text()', namespaces = root.nsmap)))
 
-    # print('redefining file_counter')
-    file_counter = file_counter + 1
+
+    file_counter = file_counter + 1             #move to next RDF file
 
 print("Authors list created!")
 
@@ -57,28 +57,32 @@ print("Authors list created!")
 # print(birthdate_list)
 # print(deathdate_list)
 
-print('authors_list = ' + str(len(authors_list)))
-print('birthdate_list = ' + str(len(birthdate_list)))
-print('deathdate_list = ' + str(len(deathdate_list)))
+# print('authors_list = ' + str(len(authors_list)))
+# print('birthdate_list = ' + str(len(birthdate_list)))
+# print('deathdate_list = ' + str(len(deathdate_list)))
 
 
-### writing to CSV file ###
 
-index_counter = 0
+
+
+
+### WRITING TO CSV FILE ###
+
+index_counter = 0               #index counter for values lists
 
 import csv
 
-outfile = open('project-gutenberg-2_dataset.csv', 'w', encoding = 'utf-8', newline = '')
+outfile = open('project-gutenberg-2_dataset.csv', 'w', encoding = 'utf-8', newline = '')        #create csv
 csvout = csv.writer(outfile)
-csvout.writerow(['author', 'birthdate', 'deathdate'])
+csvout.writerow(['author', 'birthdate', 'deathdate'])                                           #create column headers
 
 for item in authors_list:
-    author = authors_list[index_counter]
-    birthdate = birthdate_list[index_counter]
-    deathdate = deathdate_list[index_counter]
-    row = [author, birthdate, deathdate]
-    csvout.writerow(row)
-    index_counter = index_counter + 1
+    author = authors_list[index_counter]                    #assign author name to author variable
+    birthdate = birthdate_list[index_counter]               #assign birthdate to birthdate variable
+    deathdate = deathdate_list[index_counter]               #assign deathdate to deathdate variable
+    row = [author, birthdate, deathdate]                    #assign values to row
+    csvout.writerow(row)                                    #write row to csv
+    index_counter = index_counter + 1                       #move to next index
 
 outfile.close()
 print("Success: data written and file closed!")
